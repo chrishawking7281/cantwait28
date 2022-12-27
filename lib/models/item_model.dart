cf700c806486919f1295a9fa5287be91e186eb81
Krzysztof Kochel <chrishawking7281@gmail.com>
2022-12-27 15:30:41 +0100

    Separate logic to items repositories

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -1,13 +1,23 @@
+//Plik ItemModel odpowiada za wszystkie zmienne jakie mają miejsce w reszcie plików
+
+//class odpowiedzialna za wszystkie zmienne w plikach
 class ItemModel {
   ItemModel({
+    //constructor
     required this.id,
     required this.title,
     required this.imageURL,
     required this.relaseDate,
   });
 
+  //properthies
   final String id;
   final String title;
   final String imageURL;
   final DateTime relaseDate;
+
+  //methods- zczytuje ile dni zostało
+  String daysleft() {
+    return relaseDate.difference(DateTime.now()).inDays.toString();
+  }
 }
---
+++ lib/repositories/item_repositories.dart
@@ -0,0 +1,54 @@
+//repositories- czyli tu ma pobierać wszystko z zewnątrz- block jedynie odbiera i przekazuje
+
+import 'package:cantwait28/models/item_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+//class- Firebase
+class ItemsRepository {
+  Stream<List<ItemModel>> getItemsStream() {
+    return FirebaseFirestore.instance
+        .collection('items')
+        .orderBy('release_date')
+        .snapshots()
+        .map(
+      (querySnapshot) {
+        return querySnapshot.docs.map(
+          (doc) {
+            return ItemModel(
+              id: doc.id,
+              title: doc['title'],
+              imageURL: doc['image_url'],
+              relaseDate: (doc['release_date'] as Timestamp).toDate(),
+            );
+          },
+        ).toList();
+      },
+    );
+  }
+
+  //dismissible
+  Future<void> delete({required String id})  {
+    //z cubita
+     return FirebaseFirestore.instance
+          .collection('items')
+          .doc(id)
+          .delete();
+  }
+
+  Future<void> add(
+    String title,
+    String imageURL,
+    DateTime releaseDate,
+  ) async {
+    {
+      await FirebaseFirestore.instance.collection('items').add(
+        {
+          'title': title,
+          'image_url': imageURL,
+          'release_date': releaseDate,
+        },
+      );
+    }
+  }
+}
+
